c6036766f2c5e0e4235a83b1d98c4b91a5ec05d6
PELIME <1658816047@qq.com>
2019-08-29 13:51:26 +0800

    修改config配置文件

--- hello-spring-cloud-config/src/main/java/cn/pelime/hello/spring/cloud/config/ConfigApplication.java
+++ hello-spring-cloud-config/src/main/java/cn/pelime/hello/spring/cloud/config/ConfigApplication.java
@@ -2,6 +2,8 @@ package cn.pelime.hello.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ：xcx
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 1.0
  */
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class,args);
